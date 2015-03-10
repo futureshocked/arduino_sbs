e078886d48c50edad09bb2851ce61deaa9d0c408
Peter Dalmaris <peter@futureshock.com.au>
2015-03-10 17:25:23 +1100

    Updated copyright notices, fixed some formatting.

--- HowToMakeALibrary/Morse/examples/Morse_example/morse_example.ino
+++ HowToMakeALibrary/Morse/examples/Morse_example/morse_example.ino
@@ -1,3 +1,8 @@
+// Morse library example
+// 10/3/2015 by Peter Dalmaris <peter@txplore.com>
+// Source repository: https://github.com/futureshocked/arduino_sbs/tree/master/HowToMakeALibrary/Morse
+
+
 #include <Morse.h>
 
 //Morse morse = Morse(); //Just use defaults
@@ -5,11 +10,9 @@
 Morse morse = Morse(20,13); //Specify the dot duration and LED pin
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   morse.transmit("HELLO WORLD");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
