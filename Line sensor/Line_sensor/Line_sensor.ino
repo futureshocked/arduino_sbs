a4241b0bcc491f469c8e6feda3f23375ebaea300
Peter Dalmaris <peter@futureshock.com.au>
2015-06-03 17:24:42 +1000

    Update Line_sensor.ino
    The line sensor I used is marked "QA", therefore it is an analog sensor. I updated the sketch to use analogRead.

--- Line sensor/Line_sensor/Line_sensor.ino
+++ Line sensor/Line_sensor/Line_sensor.ino
@@ -1,21 +1,16 @@
-// Line Sensor Breakout - Digital
+// Line Sensor Breakout - Analog
 
 int out;
-#define tiltPin 12;
 
 void setup()
 {
   Serial.begin(9600);      // sets the serial port to 9600
-  pinMode(tiltPin, INPUT);
-
 }
 
 void loop()
 {
-  out = digitalRead(0);       // read analog input pin 0
-  
-  Serial.println(out, DEC);    // print the state of the digital pin 0
-
+  out = analogRead(0);       // read analog input pin 0
+  Serial.println(out, DEC);    // print the value of the analog pin 0
   delay(100);              // wait 100ms for next reading
 }
 
