e8edbf90cefe68fbf8dea0f406b13234353a624d
Peter Dalmaris <peter@futureshock.com.au>
2015-08-15 21:30:59 +1000

    Updated pin allocations to match the video setup.
    Thank you to Steve Miller for finding the discrepancy.

--- Shift Registers/shiftRegisters_Demo1/shiftRegisters_Demo1.ino
+++ Shift Registers/shiftRegisters_Demo1/shiftRegisters_Demo1.ino
@@ -4,11 +4,14 @@
 //Using a total of 8 LEDs
 
 //Pin connected to latch pin (ST_CP) of 74HC595
-const int latchPin = 9;
+// const int latchPin = 9;
+const int latchPin = 8;
 //Pin connected to clock pin (SH_CP) of 74HC595
-const int clockPin = 10;
+// const int clockPin = 10;
+const int clockPin = 12;
 ////Pin connected to Data in (DS) of 74HC595
-const int dataPin = 8;
+// const int dataPin = 8;
+const int dataPin = 11;
 
 void setup() {
   //set pins to output because they are addressed in the main loop
