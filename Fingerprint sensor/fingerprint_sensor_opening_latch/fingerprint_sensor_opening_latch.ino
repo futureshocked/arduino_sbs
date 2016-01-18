26150ecd7f7743ef009f8c66467dac024dec325c
Peter Dalmaris <peter@futureshock.com.au>
2016-01-19 10:47:02 +1100

    Added GPS and fingerprint sensor lectures sketches.

---
+++ Fingerprint sensor/fingerprint_sensor_opening_latch/fingerprint_sensor_opening_latch.ino
@@ -0,0 +1,95 @@
+/*************************************************** 
+  This is an example sketch for our optical Fingerprint sensor
+
+  Designed specifically to work with the Adafruit BMP085 Breakout 
+  ----> http://www.adafruit.com/products/751
+
+  These displays use TTL Serial to communicate, 2 pins are required to 
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+
+int getFingerprintIDez();
+
+// pin #2 is IN from sensor (GREEN wire)
+// pin #3 is OUT from arduino  (WHITE wire)
+SoftwareSerial mySerial(4, 5);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+// On Leonardo/Micro or others with hardware serial, use those! #0 is green wire, #1 is white
+//Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial1);
+
+unsigned long previousMillis = 0;        // will store last time LED was updated
+unsigned long currentMillis;
+const long lock_interval = 5000; //This is the amount of time I want the lock to remain open
+const int lock_pin = 9;
+
+void setup()  
+{
+
+  pinMode(lock_pin, OUTPUT);
+  digitalWrite(lock_pin,HIGH); //This will keep the latch lock to HIGH
+                        //and keep the door locked (until it goes LOW)
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  
+  Serial.begin(9600);
+  Serial.println("Adafruit finger detect test");
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1);
+  }
+  Serial.println("Waiting for valid finger...");
+}
+
+void loop()                     // run over and over again
+{
+  getFingerprintIDez();
+  delay(50);            //don't ned to run this at full speed.
+
+   currentMillis = millis();
+   //If enough time has passed since unlocking the door, lock it
+   if (currentMillis - previousMillis >= lock_interval) 
+      digitalWrite(lock_pin,HIGH); //lock the lock
+}
+
+
+// returns -1 if failed, otherwise returns ID #
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+
+  //IF there is a confident match, open the latch lock
+  //and keep it open for the unlock interval
+  if (finger.confidence > 50)
+  {
+    previousMillis = millis();    
+    digitalWrite(lock_pin,LOW);  //open the lock
+    }
+  
+  return finger.fingerID; 
+}
